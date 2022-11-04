0d6bb3980535e450bc2fb42018713cdee7f4a8cd
2ez4rtz <ggg573608@gmail.com>
2022-11-03 23:10:39 -0700

    adding new logo nav to home page for each page , scroll down slowly....etc

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function changeImg(n) {
 }
 
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
